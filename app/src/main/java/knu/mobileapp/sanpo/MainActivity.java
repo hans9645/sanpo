b0fc666a4404a549fdc57b58f6bc2781dfd21b7b
hans9645 <61701428+hans9645@users.noreply.github.com>
2020-12-12 02:28:34 +0900

    add function code that can set name at MainActivity.java

--- app/src/main/java/knu/mobileapp/sanpo/MainActivity.java
+++ app/src/main/java/knu/mobileapp/sanpo/MainActivity.java
@@ -1,19 +1,27 @@
 package knu.mobileapp.sanpo;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private View drawerView;
 
+
+
+    Button btn_dialog;
+    TextView tv_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +55,44 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+
+        btn_dialog=(Button)findViewById(R.id.btn_dialog);
+        tv_result=(TextView)findViewById(R.id.tv_result);
+
+        btn_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder ad= new AlertDialog.Builder(MainActivity.this);
+                ad.setIcon(R.mipmap.ic_launcher);
+                ad.setTitle("설정");
+                ad.setMessage("반려동물의 이름을 무엇으로 설정하겠습니까?");
+
+
+                final EditText et= new EditText(MainActivity.this);
+                ad.setView(et);
+
+                ad.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String result=et.getText().toString();
+                        tv_result.setText(result);
+                        dialog.dismiss();
+                    }
+                });
+
+                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                ad.show();//다이얼로그가 정상적으로 뜨게 하려고 하는 거
+
+            }
+        });
+
     }
 
     DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
