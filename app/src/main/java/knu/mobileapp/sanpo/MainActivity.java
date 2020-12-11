aa7eda0df5deefae4eb67cb227bdc53c9b6668fa
hans9645 <61701428+hans9645@users.noreply.github.com>
2020-12-12 01:52:22 +0900

    MainActivity.java commit again

--- app/src/main/java/knu/mobileapp/sanpo/MainActivity.java
+++ app/src/main/java/knu/mobileapp/sanpo/MainActivity.java
@@ -1,14 +1,74 @@
 package knu.mobileapp.sanpo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerView=(View)findViewById(R.id.drawer);
+
+
+        Button btn_open=(Button)findViewById(R.id.btn_open);//open버튼 눌려졌을 때 메뉴 레이아웃 열리도록함.
+        btn_open.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+
+        Button btn_close=(Button)findViewById(R.id.btn_close);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
     }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
+
 }
